b9b0baab41db729ac156def99cfaa8556cd8dcd0
Lakshay-Bansal <84389082+Lakshay-Bansal@users.noreply.github.com>
2023-05-10 22:22:40 +0530

    Add files via upload

---
+++ pages/2_Dynamic_Calibration.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 27 12:16:27 2023
+
+@author: lakshayb
+"""
+
+import streamlit as st
+import matplotlib.pyplot as plt
+
+st.title("Need of camera calibration")
+
+
+
