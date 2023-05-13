038d1e936ff037b8159cb50cebe88f43f3060180
Lakshay-Bansal <84389082+Lakshay-Bansal@users.noreply.github.com>
2023-05-13 18:00:55 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,66 +8,30 @@ Created on Thu Apr 27 12:16:27 2023
 
 import streamlit as st
 import matplotlib.pyplot as plt
+import os
+import sys
 
-st.title("Camera Calibration")
-st.text("We can work with two method of camera calibration:")
-st.text("1. Calibration Pattern")
-st.text("2. Dynamic Scene for calibration parameter correction")
+PWD = os.getcwd()
+sys.path.append(rf'{PWD}\Images')
 
-# fig, ax = plt.subplots()
-# ax.scatter([1, 2, 3], [1, 2, 3])
-# plt.title("Try scatter plot")
-# plt.xlabel("Time")
-# plt.ylabel("Amount")
-# st.pyplot(fig)
+st.title("About")
 
-# # st.altair_chart(my_chart)
+st.write('''
+Project explore the estimation of the camera parameters such as intrinsic (focal length, camera offset) and 
+extrinsic parameters (rotation and translation of stereo setup) of the stereo camera.
+''')
 
-# # st.image("./../Single Capture Algo.png")
+st.text("We can classify camera calibration in two broad types:")
+st.text("1. Factory Calibration")
+st.text("2. Dynamic Calibration")
 
-# st.graphviz_chart("""
-#                   digraph{
-#                       "Capture image" -> "Feature extraction"
-#                       "Feature extraction" -> "Feature matching" 
-#                       "Feature matching" -> "Epipolar estimation"
-#                       "Epipolar estimation" -> "Bundle Adjustment"}
-#                   """)
+st.image('Images/flow_calib.jpg', 
+         caption='''Figure : Types of camera calibration.''',
+                width=400)
 
-
-# path_left = st.text_input("Add path of images captured by left camera of stereo setup")
-# path_right = st.text_input("Add path of images captured by right camera of stereo setup")
-
-# left, right = st.columns(2)
-
-# choice = left.button("Dynamic calibration")
-# choice = right.button("Calibration pattern based calibration")
-
-# if choice == "Dynamic calibration":
-#     st.write("It uses dynaic real scene for calibration")
-
-# if choice == "Calibration pattern based calibration":
-#     st.write("Use fixed checkerboard pattern for camera calibration")
-
-
-# method = st.radio("Select the method", ["Dynamic scene", "Checkerboard pattern"], index=0)
-# st.write("You had selected", method)
-
-# interval = st.slider("Interval", min_value=1, max_value=5, value=1, step=1)
-
-# st.write(interval)
-
-
-# navig = st.sidebar.radio("Navigation", ["Home", "Theory", "Dynamic Calibration", "Single Capture Calibration", "Inbuilt Function Calibration"])
-
-# if navig == "Theory":
-#     st.write("Why????")
-#     st.balloons() # use when calibration result are calculated
-
-
-
-# method = st.sidebar.selectbox("Select method", ["Dynamic scene", "Checkerboard pattern"])
-# if method == "Dynamic scene":
-#     st.write("Awesome")
-
-# if method == "Checkerboard pattern":
-#     st.write("Obselete")
+st.write('''
+1. Factory calibration - Camera parameters are estimated by using checkerboard pattern 
+so as to make 3D measurement (depth) more accurate by performing bundle adjustment.
+2. Dynamic calibration - Calibration using real scene images for correcting the possible deformation due to
+transportation and handling.
+''')
